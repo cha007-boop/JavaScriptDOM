1ce4130e9da1aae8b7df49d68390c57bcff1d0a4
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2026-09-22 09:47:43 +0200

    opgave 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,24 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+// function Sum(a,b){
+//     return a + b;
+// }
+
+// console.log(Sum(5,10));
+// console.log(Sum(5,"10"));
+
+// Opgave 1
+function CreateLiElement(elementText, classAttribute, id){
+    let newElement = document.createElement("li");
+    newElement.setAttribute("class", classAttribute);
+    newElement.setAttribute("id", id);
+
+    let textNode = document.createTextNode(elementText);
+    newElement.appendChild(textNode);
+
+    return newElement;
+}
+
+let liste = document.getElementById("list");
+liste.appendChild(CreateLiElement("Banana", "healthy", "4"));
